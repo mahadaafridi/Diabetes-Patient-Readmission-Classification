905ad56cfa08779f07d5fdd12343eda2d6246a29
Mahad Afridi <mahadakbarafridi@gmail.com>
2024-05-30 11:57:13 -0700

    dataset imported

---
+++ main.py
@@ -0,0 +1,15 @@
+from ucimlrepo import fetch_ucirepo 
+
+
+# # fetch dataset 
+# diabetes_130_us_hospitals_for_years_1999_2008 = fetch_ucirepo(id=296) 
+  
+# # data (as pandas dataframes) 
+# X = diabetes_130_us_hospitals_for_years_1999_2008.data.features 
+# y = diabetes_130_us_hospitals_for_years_1999_2008.data.targets 
+  
+# # metadata 
+# print(diabetes_130_us_hospitals_for_years_1999_2008.metadata) 
+  
+# # variable information 
+# print(diabetes_130_us_hospitals_for_years_1999_2008.variables)
