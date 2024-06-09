bb5d258f8347153f9d8954475a066824f2c15ba2
Mahad Afridi <mahadakbarafridi@gmail.com>
2024-06-09 07:54:35 -0700

    Mahads Logistic classifier final stuff

--- logistic_classifier.py
+++ logistic_classifier.py
@@ -10,7 +10,6 @@ diabetes_130_us_hospitals_for_years_1999_2008 = fetch_ucirepo(id=296)
 
 X = diabetes_130_us_hospitals_for_years_1999_2008.data.features
 y = diabetes_130_us_hospitals_for_years_1999_2008.data.targets
-
 enc = OneHotEncoder(handle_unknown='ignore')
 # print(enc.fit_transform(X))
 mapping = {'NO': 0, '<30': 1, '>30': 2}
@@ -55,56 +54,56 @@ X_test_scaled = scaler.transform(X_test)
 # print(best_params)
 ## END
 
-# ##   DIFFERENT LEARNING SIZES 
-def errors_for_train_sizes_lr(X_tr: np.array, y_tr: np.array, X_te: np.array, y_te: np.array, train_sizes: list[int]) -> tuple[list, list, list, list]:    
-        # append error rates to the following lists
-        tr_err_lr = [] # training error rates for Logistic Regression
-        te_err_lr = [] # testing error rates for Logistic Regression
-        accuracy_list = []
+##   DIFFERENT LEARNING SIZES 
+# def errors_for_train_sizes_lr(X_tr: np.array, y_tr: np.array, X_te: np.array, y_te: np.array, train_sizes: list[int]) -> tuple[list, list, list, list]:    
+#         # append error rates to the following lists
+#         tr_err_lr = [] # training error rates for Logistic Regression
+#         te_err_lr = [] # testing error rates for Logistic Regression
+#         accuracy_list = []
 
-        for size in train_sizes:
-                X_tr_sample = X_tr[:size, :]
-                y_tr_sample = y_tr[:size]
-                X_te_sample = X_te[:size, :]
-                y_te_sample = y_te[:size]
-                model = LogisticRegression(multi_class='multinomial', solver='newton-cg', max_iter=1000, C=.01)
+#         for size in train_sizes:
+#                 X_tr_sample = X_tr[:size, :]
+#                 y_tr_sample = y_tr[:size]
+#                 X_te_sample = X_te[:size, :]
+#                 y_te_sample = y_te[:size]
+#                 model = LogisticRegression(multi_class='multinomial', solver='newton-cg', max_iter=1000, C=.01)
 
-                model.fit(X_tr_sample, y_tr_sample)
+#                 model.fit(X_tr_sample, y_tr_sample)
 
-                y_pred = model.predict(X_te_sample)
+#                 y_pred = model.predict(X_te_sample)
 
-                accuracy = accuracy_score(y_te_sample, y_pred)
+#                 accuracy = accuracy_score(y_te_sample, y_pred)
 
-                tr_err = 1 - model.score(X_tr_sample, y_tr_sample)
-                te_err = 1 - model.score(X_te_sample , y_te_sample)
-                accuracy_list.append(accuracy)
-                tr_err_lr.append(tr_err)
-                te_err_lr.append(te_err)
+#                 tr_err = 1 - model.score(X_tr_sample, y_tr_sample)
+#                 te_err = 1 - model.score(X_te_sample , y_te_sample)
+#                 accuracy_list.append(accuracy)
+#                 tr_err_lr.append(tr_err)
+#                 te_err_lr.append(te_err)
 
 
-        return tr_err_lr, te_err_lr, accuracy_list 
+#         return tr_err_lr, te_err_lr, accuracy_list 
 
-train_sizes = [50, 500, 2000, 5000, 10000, 20000, 40000, X_train.shape[0]] 
+# train_sizes = [50, 500, 2000, 5000, 10000, 20000, 40000, X_train.shape[0]] 
 
-tr_err_lr, te_err_lr, acc_list = errors_for_train_sizes_lr(X_train, y_train, X_test, y_test, train_sizes)
+# tr_err_lr, te_err_lr, acc_list = errors_for_train_sizes_lr(X_train, y_train, X_test, y_test, train_sizes)
 
-print(tr_err_lr)
-print(te_err_lr)
-print(acc_list)
+# print(tr_err_lr)
+# print(te_err_lr)
+# print(acc_list)
 
-plt.figure(figsize=(10, 6))
+# plt.figure(figsize=(10, 6))
 
-plt.semilogx(train_sizes, tr_err_lr, label='training error', color='black')
-plt.semilogx(train_sizes, te_err_lr, label='testing error', color='purple')
-plt.semilogx(train_sizes, acc_list, label='accuracy', color='red')
+# plt.semilogx(train_sizes, tr_err_lr, label='Training Error', color='black')
+# plt.semilogx(train_sizes, te_err_lr, label='Testing Error', color='purple')
+# plt.semilogx(train_sizes, acc_list, label='Accuracy', color='red')
 
-plt.xlabel('train size')
-plt.ylabel('accuracy or error rate')
-plt.title('trainig size vs error and accuracy')
-plt.legend()
-plt.grid(True)
-plt.show()
-# ##     END
+# plt.xlabel('Training Size')
+# plt.ylabel('Accuracy or Error Rate')
+# plt.title('Training Size vs Accuracy or Error Rate')
+# plt.legend()
+# plt.grid(True)
+# plt.show()
+##     END
 
 
 
